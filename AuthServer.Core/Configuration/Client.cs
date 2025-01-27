36072a5342f3bd657af029449548bc4d0d0ad006
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-27 14:43:18 +0300

    ITokenService initialize

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configuration
+{
+    /// <summary>
+    /// Authorization server'a istek yapacak clientlara karşılık gelen sınıf
+    /// </summary>
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+       
+        /// <summary>
+        /// Hangi api'ye erişim izni var bilgisini tutar
+        /// </summary>
+        public List<String> Audiences { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+
+namespace AuthServer.Core.Services
+{
+    /// <summary>
+    /// Uygulama içerisinde kullanılacak token servisini temsil eder
+    /// </summary>
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
