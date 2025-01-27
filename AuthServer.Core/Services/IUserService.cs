ee1a30d30d6c0d8e6288b8874a2bdeaaff53418e
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-27 15:51:05 +0300

    IUserService initialize

---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthServer.Core.DTOs;
+using SharedLibary.DTOs;
+
+namespace AuthServer.Core.Services
+{
+    /// <summary>
+    /// Indentity kütüphanesi kullanacağımız için sadece servis oluşturuyoruz, bu servis ile kullanıcı kayıt işlemlerini yapacağız.
+    /// </summary>
+    public interface IUserService
+    {
+        Task <Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByNameAsync(string userName);
+
+    }
+}
