248bd2408f24e63a24896892439a2890f0192213
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-27 16:43:38 +0300

    IUnitOfWork -- Core Layer complate

---
+++ AuthServer.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.UnitOfWork
+{
+    /// <summary>
+    /// İşlemleri tek bir transaction altında toplamak için kullanılır.
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
