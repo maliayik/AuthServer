87c74738f2ea142bf3bdd335f40f173009fa03e6
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-27 13:15:47 +0300

    ErrorDTO initialize

---
+++ SharedLibary/DTOs/ErrorDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibary.DTOs
+{
+    //Tüm API'lerde dönülecek hataların tutulduğu ortak sınıf
+    public class ErrorDto
+    {
+        //başka bir sınıftan set edilmesini engellemek için private set kullanıldı
+        public List<string> Errors { get; private set; }
+        public bool IsShow { get; private set; }//client tarafında hata mesajlarını göstermek isteyip istemediğini belirleyen property
+
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        //tek hata durumları için
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+
+        //birden fazla hata durumları için
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+
+    }
+}
