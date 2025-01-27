5a2326a26c27e2e7638ecf31b69a189d60709e5c
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-27 20:36:40 +0300

    GenericRepository initialize

---
+++ AuthServer.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using AuthServer.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet; //tabloları temsil eder
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task AddAsync(TEntity entity) => await _dbSet.AddAsync(entity);
+
+        //tüm datayı çekmek istediğimizde sorgulamayacağımız için IQueryable değil IEnumerable kullandık, ihtiyaca göre değişebilir.
+        public async Task<IEnumerable<TEntity>> GetAllAsync() => await _dbSet.ToListAsync();
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached; //memoryde yer kaplamasın diye detached yapıyoruz.
+            }
+            return entity;
+        }
+
+        public void Remove(TEntity entity) => _dbSet.Remove(entity);
+
+
+        public TEntity Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            return entity;
+        }
+
+        public IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate) => _dbSet.Where(predicate);
+
+    }
+}
