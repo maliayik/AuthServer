b9d9ba6be0ba38524faca64c0f91add661fe7002
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-27 15:13:36 +0300

    IAuthenticationService initialize

---
+++ AuthServer.Core/DTOs/LoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.DTOs
+{
+    /// <summary>
+    /// Kullanıcı giriş işlemleri sonrası login durumuna göre token üretmek için kullanılacak model
+    /// </summary>
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthServer.Core.DTOs;
+using SharedLibary.DTOs;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        /// <summary>
+        /// Kullanıcı adı ve şifre ile token oluşturma işlemi
+        /// </summary>
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        /// <summary>
+        /// Reflesh token ile yeni token oluşturma işlemi
+        /// </summary>
+        Task<Response<TokenDto>> CreateTokenByRefleshToken(string refleshToken);
+
+        /// <summary>
+        /// Reflesh tokeni veritabanından silme işlemi
+        /// </summary>
+        Task<Response<NoContentDto>> RevokeRefleshToken(string refleshToken);
+
+        /// <summary>
+        /// Tanımlı bir client için token oluşturma işlemi
+        /// </summary>
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
