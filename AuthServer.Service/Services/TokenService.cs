45be2ec9b21638104580dc94f3c90a3df1411b60
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-28 11:49:34 +0300

    TokenService - CreateRefleshToken() added

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibary.Configurations;
+
+namespace AuthServer.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly CustomTokenOption _tokenOption;
+
+
+
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOption> options )
+        {
+            _userManager = userManager;
+            _tokenOption = options.Value;
+        }
+
+        /// <summary>
+        /// Refresh token oluşturur
+        /// </summary>
+        private string CreateRefreshToken()
+        {
+            var numberByte = new Byte[32];
+            using var rnd = RandomNumberGenerator.Create();
+            rnd.GetBytes(numberByte);
+
+            return Convert.ToBase64String(numberByte);
+        }
+
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
