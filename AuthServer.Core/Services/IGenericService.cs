1ef906e4e28b60d21742b250b16fe1b860217d2f
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-28 10:53:38 +0300

    GenericService added

--- AuthServer.Core/Services/IGenericService.cs
+++ AuthServer.Core/Services/IGenericService.cs
@@ -9,13 +9,13 @@ using SharedLibary.DTOs;
 namespace AuthServer.Core.Services
 {
     //Entityi Dto'ya çevirip döndüren metotlar burada olacak
-    public interface IServiceGeneric<TEntity, TDto> where TEntity : class where TDto : class
+    public interface IGenericService<TEntity, TDto> where TEntity : class where TDto : class
     {
         Task<Response<TDto>> GetByIdAsync(int id);
         Task<Response<IEnumerable<TDto>>> GetAllAsync();
         Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
-        Task<Response<TDto>> AddAsync(TEntity entity);
-        Task<Response<NoContentDto>> Remove(TEntity entity);
-        Task<Response<NoContentDto>> Update(TEntity entity);
+        Task<Response<TDto>> AddAsync(TDto dto);
+        Task<Response<NoContentDto>> Remove(int id);
+        Task<Response<NoContentDto>> Update(TDto dto,int id);
     }
 }
