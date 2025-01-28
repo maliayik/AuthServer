6119c917013a60399acdc9ad3803091817034c1e
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-28 11:16:36 +0300

    SignService (symetric key) added

---
+++ AuthServer.Service/Services/SignService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthServer.Service.Services
+{
+    // Bu klassın amacı, token oluştururken kullanılacak olan imzayı için simetrik security key'i oluşturmak.
+    internal static class SignService
+    {
+        public static SecurityKey GetSymetricSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
