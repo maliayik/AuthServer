bccf840a22842a27f759ebd49ebfe408be8dc40b
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-28 21:31:50 +0300

    AuthController

---
+++ AuthServer.API/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController(IAuthenticationService authenticationService, IUserService userService) : CustomBaseController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(LoginDto loginDto)
+        {
+            var result = await authenticationService.CreateTokenAsync(loginDto);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            var result = authenticationService.CreateTokenByClient(clientLoginDto);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RevokeRefreshToken(RefleshTokenDto refleshTokenDto)
+        {
+            var result = await authenticationService.RevokeRefleshToken(refleshTokenDto.Token);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshToken(RefleshTokenDto refleshTokenDto)
+        {
+            var result = await authenticationService.CreateTokenByRefleshToken(refleshTokenDto.Token);
+
+            return ActionResultInstance(result);
+        }
+    }
+}
---
+++ AuthServer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibary.DTOs;
+
+namespace AuthServer.API.Controllers
+{
+    public class CustomBaseController : ControllerBase
+    {
+        /// <summary>
+        /// Response tipinde dönüş yapacak tüm controllerlar için ortak metot
+        /// </summary>
+        public IActionResult ActionResultInstance<T>(Response<T> response) where T : class
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
