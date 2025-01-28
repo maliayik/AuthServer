1c94a721e51f7b54a4fd7d26e14413e773fa4923
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-28 11:36:11 +0300

    Options Pattern added

--- AuthServer.API/Program.cs
+++ AuthServer.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Configuration;
+using SharedLibary.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOptions"));
 
 var app = builder.Build();
 
---
+++ SharedLibary/Configurations/CustomTokenOption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibary.Configurations
+{
+    //appsettings.json dosyasındaki TokenOptions alanlarına karşılıl gelen sınıf
+    public class CustomTokenOption
+    {
+        public List<string> Audience { get; set; }
+        public string Issuer { get; set; }
+        public int AccessTokenExpiration { get; set; }
+        public int RefleshTokenExpiration { get; set; }
+        public string SecurityKey { get; set; }
+    }
+}
