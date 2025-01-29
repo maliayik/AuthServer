8362c7bd5ac36fa1ba5fe93f6fc9698b95096e8d
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-29 14:57:12 +0300

    Product Controller added

---
+++ AuthServer.API/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(IGenericService<Product, ProductDto> productService) : CustomBaseController
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduct()
+        {
+            return ActionResultInstance(await productService.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await productService.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await productService.Update(productDto, productDto.Id));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task <IActionResult> DeleteProduct(int id)
+        {
+            return ActionResultInstance(await productService.Remove(id));
+        }
+    }
+}
--- AuthServer.Core/DTOs/ProductDto.cs
+++ AuthServer.Core/DTOs/ProductDto.cs
@@ -11,6 +11,8 @@ namespace AuthServer.Core.DTOs
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public int Stock { get; set; }
         public string UserId { get; set; }
+        
     }
 }
