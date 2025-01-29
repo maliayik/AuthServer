5b2028c920756c0153f99893e883a93120d52be7
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-29 17:16:53 +0300

    fix

--- SharedLibary/DTOs/ErrorDto.cs
+++ SharedLibary/DTOs/ErrorDto.cs
@@ -12,13 +12,9 @@ namespace SharedLibary.DTOs
     public class ErrorDto
     {
         //başka bir sınıftan set edilmesini engellemek için private set kullanıldı
-        public List<string> Errors { get; private set; }
+        public List<string> Errors { get; private set; } = new List<string>();
         public bool IsShow { get; private set; }//client tarafında hata mesajlarını göstermek isteyip istemediğini belirleyen property
 
-        public ErrorDto()
-        {
-            Errors = new List<string>();
-        }
 
         //tek hata durumları için
         public ErrorDto(string error, bool isShow)
